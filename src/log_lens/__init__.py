62a1158969f1e5d0b1a8557eb8e3e616cd1c3431
Beau Bremer <beau.bremer@gmail.com>
2025-12-25 19:53:27 -0600

    chore: remove legacy src/ directory

--- src/log_lens/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Log Lens - Lightweight CLI for analyzing Apache/Nginx server logs."""
-
-__version__ = "0.4.0"
-__author__ = "KnowOneActual"
-__description__ = "Lightweight CLI for analyzing Apache/Nginx server logs"
-
-__all__ = ["__version__", "__author__", "__description__"]
